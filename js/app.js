74cd058fba4d10554a66c98f9cc9e9a87285bf47
Lajos Nagy <lajos.j.nagy@gmail.com>
2024-02-19 23:29:41 +0100

    hamburger menu with animation and media query

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // page copyright year
 const $year = document.querySelector('#year').innerHTML = new Date().getFullYear();
+
+// hamburger menu
+const $hamburgerMenu = document.querySelector('.js-hamburger-menu');
+const $hamburgerMenuIcon = document.querySelector('.js-hamburger-menu-icon');
+
+$hamburgerMenu.addEventListener('click', () => {
+    $hamburgerMenuIcon.classList.toggle('active');
+});
