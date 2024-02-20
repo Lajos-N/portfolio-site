2bb9b4390e205ca2ed68118fc0c50436de972861
Lajos Nagy <lajos.j.nagy@gmail.com>
2024-02-20 22:51:47 +0100

    drop down menu complete

--- js/app.js
+++ js/app.js
@@ -4,7 +4,11 @@ const $year = document.querySelector('#year').innerHTML = new Date().getFullYear
 // hamburger menu
 const $hamburgerMenu = document.querySelector('.js-hamburger-menu');
 const $hamburgerMenuIcon = document.querySelector('.js-hamburger-menu-icon');
+const $hamburgerMenuList = document.querySelector('nav ul');
+const $bodyScrolling = document.querySelector('body');
 
 $hamburgerMenu.addEventListener('click', () => {
     $hamburgerMenuIcon.classList.toggle('active');
+    $hamburgerMenuList.classList.toggle('is-active');
+    $bodyScrolling.classList.toggle('stop-scrolling');
 });
